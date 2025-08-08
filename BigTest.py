bb92f47d51587a5d94735cf53b54e3d41f3ad1df
MrKomron <vosidov.komron@gmail.com>
2025-08-08 03:49:29 +0200

    BigTest.py week 4 vrijdag

--- BigTest.py
+++ BigTest.py
@@ -1,169 +1,160 @@
 # Vraag 1
-# def midden(p1,p2):
-#     result=((p1[0]+p2[0])/2,(p1[1]+p2[1])/2)
-#     return result
+# def splits(l):
+#     mid = len(l) // 2
+#     if len(l)%2==0:
+#         left=l[:mid]
+#         right=l[mid:]
+#     else:
+#         left=l[:mid]
+#         right=l[mid+1:]
+#     return (left, right)
 
 # Vraag 2
-# def midden(A,B):
-#     temp_result=[]
-#     for a,b in zip(A,B):
-#         temp_result.append((a+b)/2)
-#     return tuple(temp_result)
-
-# Vraag 3
-# def tel_getallen(l):
-#     max_getal = max(l)
-#     frequenties = [0] * (max_getal + 1)  # Maak lijst van nullen
-#     for getal in l:
-#         frequenties[getal] += 1  # Verhoog de teller op de juiste plek
-#     return frequenties
+# from math import sqrt
+# def is_prime(n):
+#     """
+#     check whether n is a prime number
+#     :param n: the number being tested
+#     :return: True iff the number is prime
 #
-# l = [1, 2, 5, 3, 2, 17, 5, 6, 7, 3, 2, 1, 9, 3]
-# frequenties = tel_getallen(l)
-# print(frequenties)
+#     # >>> is_prime(2)
+#     True
+#     # >>> is_prime(4)
+#     False
+#     """
+#     if n < 2:
+#         return None
+#
+#     for i in range(2,int(n**0.5)+1):
+#         if n % i == 0:
+#             return False
+#
+#     return True
+# print(is_prime(5))
+# print(is_prime(2))
+# print(is_prime(8))
+# print(is_prime(29))
+# print(is_prime(101))
+# print(is_prime(25))
+# print(is_prime(487))
+
+# Vraag 3 / Maar wat als we niet weten s,w en h?
+# def item_order(bestellingen):
+#     s=bestellingen.count("s")
+#     h=bestellingen.count("h")
+#     w=bestellingen.count("w")
+#     result=[s,h,w]
+#     return result
+# print(item_order(["s", "w", "h", "s", "h"]))
+# print(item_order(["h", "w", "h"]))
+# print(item_order(["s", "h", "w", "h", "h", "w"]))
+# print(item_order(["s"]))
 
 # Vraag 4
-# from alice_book import book
-# def tel_woorden(l):
-#     result={}
-#     teller=0
-#     for i in l:
-#         if i not in result:
-#             result[i]=1
-#         else:
-#             result[i]+=1
-#
-#     return result
-#
-# woorden = book.split()
-# print(tel_woorden(woorden))
+# def place_order(db,email,order):
+#     db[email]=item_order(order)
 
 # Vraag 5
-# def print_frequenties(dict):
-#     for woord,aantal in dict.items():
-#         print(woord, aantal)
+# def print_previous_order(db,email):
+#     if email not in db:
+#         print("Je hebt nog geen bestellingen geplaatst.")
+#         return
+#
+#     bestelling=db[email]
+#     items=["salade","hamburger","water"]
+#     for count, item in zip(bestelling,items):
+#         print(f"Je bestelde {count} {item}(s).")
+#
+# db = {}
+# print_previous_order(db,"jefke@gmail.com")
+# place_order(db,"jefke@gmail.com",["s", "w", "h", "s", "h"])
+# place_order(db,"mieke@gmail.com",["w", "w", "h", "s", "h"])
+# print_previous_order(db,"jefke@gmail.com")
+# print_previous_order(db,"mieke@gmail.com")
 
 # Vraag 6
-def list2dict(l1,l2):
-    result={}
-    for key,value in zip(l1,l2):
-        if len(l1)==len(l2):
-            result[key]=value
-        else:
-            return None
-    return result
-
-print(list2dict(['Ten', 'Twenty', 'Thirty'],[10, 20, 30]))
-print(list2dict(['Ten', 'Twenty', 'Thirty'],[10, 20]))
-print(list2dict(['Ten', 'Ten', 'Thirty'],[10, 20]))
-print(list2dict([1,2,3,4],[5,6,7,8]))
+# def create_dice():
+#     dice=[]
+#     for i in range(1,14):
+#         dice.append(set())
+#     return dice
 
 # Vraag 7
-# def bepaal_winnaar(data):
-#     counter=None
-#     winnaar=None
-#     for key, value in data.items():
-#         som=sum(value)
-#         if counter is None or som<counter:
-#             counter=som
-#             winnaar=key
-#     return winnaar
-#
-# punten = { "Calders": [2, 4, 3, 2, 3, 4, 4, 4, 3, 3], # Som is 32
-#            "Hofkens": [3, 3, 2, 4, 2, 4, 3, 3, 4, 3], # Som is 31
-#            "Laenens": [2, 3, 4, 2, 3, 4, 4, 3, 4, 3], # Som is 32
-#            "Symens": [2, 3, 2, 4, 2, 2, 3, 2, 2, 4] }# Som is 26 }
-# print(bepaal_winnaar(punten))
+# def create_dice():
+#     dice=[]
+#     for i in range(1,14):
+#         dice.append(set())
+#     for a in range(1,7):
+#         for b in range(1,7):
+#             s=a+b
+#             dice[s].add((a,b))
+#     return dice
 
 # Vraag 8
-# def tel_elementen(l):
-#     return len(set(l))
-
+# def lose():
+#     dice=create_dice()
+#     return dice[2] | dice[3] | dice[12]
+#
+# def win():
+#     dice=create_dice()
+#     return dice[7] | dice[11]
 
 # Vraag 9
-# def is_priem(getal):
-#     if getal < 2:
-#         return False
-#     for i in range(2, int(getal**0.5)+1):
-#         if getal % i == 0:
-#             return False
-#     return True
-#
-# def volgend_priemgetal(getal):
-#     volgende = getal + 1
-#     while not is_priem(volgende):
-#         volgende += 1
-#     return volgende
-#
-#
-# def priemgetallen(n):
-#     start=2
-#     result=[]
-#     while len(result)<n:
-#         if is_priem(start):
-#             result.append(start)
-#         start+=1
-#     return result
+# from random import randint, random
+# def roll_dice():
+#     left=random.randint(1,6)
+#     right=random.randint(1,6)
+#     return (left,right)
 
 # Vraag 10
-# def fibonacci(n):
-#     result=[1,1]
-#     new_item=0
-#     while len(result)<n:
-#         new_item=result[-1]+result[-2]
-#         result.append(new_item)
-#     return result
-# print(fibonacci(6))
+# def game_ends(worp):
+#     total=sum(worp)
+#     return total in {2,3,7,11,12}
+
 
 # Vraag 11
-# def priem_en_fib(n):
-#     return set(priemgetallen(n)) & set(fibonacci(n))
+# def minimum_elements(l):
+#     if not l:
+#         return None
+#     counter=0
+#     min_val=l[0]
+#     for i in range(1,len(l)):
+#         if l[i]<min_val:
+#             min_val=l[i]
+#             counter=1
+#         elif l[i]==min_val:
+#             counter+=1
+#     return (min_val, counter)
+#
+# print(minimum_elements([3,2,4,3,1,1,3,2,1]))
+# print(minimum_elements([1,3,5,4,3,1,2]))
+# print(minimum_elements([5,3,6,5,7,4,2]))
+# print(minimum_elements([10**7,10**6,10**6,10**6]))
 
 # Vraag 12
-# def priem_of_fib(n):
-#     return set(priemgetallen(n)) | set(fibonacci(n))
+# def add_matrices(m1,m2):
+#     result=[]
+#     for row1, row2 in zip(m1,m2):
+#         if len(row1) != len(row2):
+#             return None
+#
+#         new_row=[]
+#         for val1,val2 in zip(row1,row2):
+#             new_row.append(val1+val2)
+#         result.append(new_row)
+#     return result
 
 # Vraag 13
-# def priem_xof_fib(n):
-#     return set(priemgetallen(n)) ^ set(fibonacci(n))
-
-# Vraag 14
-# Function to add a new task
-# def add_task(tasks, task):
-#     tasks.append(task)
+# def multiply_matrices(a,b):
+#     if len(a[0]) != len(b):
+#         return None
+#     m=len(a)
+#     n=len(a[0])
+#     p=len(b[0])
+#     result=[[0 for _ in range(p)] for _ in range(m)]
 #
-# # Function to update a task's status
-# def update_task(tasks, task, status):
-#     for t in tasks:
-#         if t['name'] == task:
-#             t['status'] = status
-#
-# # Function to print all pending tasks
-# def print_pending_tasks(tasks):
-#     pending_tasks = []
-#     for task in tasks:
-#         if task['status'] == 'pending':
-#             print(task['name'])
-#
-# # Function to check if a task is complete
-# def is_task_complete(tasks, task):
-#     for t in tasks:
-#         if t['name'] == task:
-#             return t['status'] == 'complete'
-#     return None
-#
-# tasks = []
-# # Adding tasks
-# add_task(tasks, {'name': 'Task 1', 'status': 'pending'})
-# add_task(tasks, {'name': 'Task 2', 'status': 'complete'})
-# add_task(tasks, {'name': 'Task 3', 'status': 'pending'})
-#
-# print_pending_tasks(tasks)
-#
-# update_task(tasks, 'Task 1', 'complete')
-#
-# print_pending_tasks(tasks)
-#
-# print("Is Task 1 complete?", is_task_complete(tasks, 'Task 1'))
-# print("Is Task 3 complete?", is_task_complete(tasks, 'Task 3'))
-# print("Is Task 4 complete?", is_task_complete(tasks, 'Task 4'))
+#     for i in range(m):
+#         for j in range(p):
+#             for k in range(n):
+#                 result[i][j]+=a[i][k]*b[k][j]
+#     return result
